4a1d6d1940660a2b6a0c78796a96553d639549e7
Fauzanyusuf <44010289+Fauzanyusuf@users.noreply.github.com>
2025-09-26 00:25:56 +0700

    fix: update server listen method to include HOST parameter for improved configuration

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,12 +1,12 @@
 import { app } from "./application/app.js";
 import { logger } from "./application/logging.js";
 import dotenv from "dotenv";
-
+    
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   logger.info(`Server is running on http://${HOST}:${PORT}`);
 });
