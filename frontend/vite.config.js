22c60fd588973a71829c5343927131a84e56aa9f
Fauzanyusuf <44010289+Fauzanyusuf@users.noreply.github.com>
2025-10-14 19:32:26 +0700

    refactor: simplify Vite configuration by removing unused Babel plugin and cleaning up plugin structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,31 +8,33 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (!id.includes("node_modules")) return;
-          if (id.includes("node_modules/react-dom")) return "react-dom";
-          if (id.includes("node_modules/react")) return "react";
-          const match = id.match(/node_modules\/([^/]+)\/?.*/);
-          if (match && match[1]) return `pkg-${match[1]}`;
-        },
-      },
-    },
-    chunkSizeWarningLimit: 700,
-  },
+	plugins: [
+		tailwindcss(),
+		react(),
+		// {
+		// babel: {
+		//   plugins: [["babel-plugin-react-compiler"]],
+		// },
+
+		// }
+	],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (!id.includes("node_modules")) return;
+					if (id.includes("node_modules/react-dom")) return "react-dom";
+					if (id.includes("node_modules/react")) return "react";
+					const match = id.match(/node_modules\/([^/]+)\/?.*/);
+					if (match && match[1]) return `pkg-${match[1]}`;
+				},
+			},
+		},
+		chunkSizeWarningLimit: 700,
+	},
 });
